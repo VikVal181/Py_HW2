6290589f71b26dc7cc732a106be1a5b4bd2f066c
Viktoriia Valuykina <v.valuykina@air-burg.ru>
2023-02-04 23:40:35 +0300

    improved solution

--- Task1.py
+++ Task1.py
@@ -4,11 +4,15 @@
 # 6782 -> 23
 # 0,56 -> 11
 
-number = (input('Введите число: '))
-sum = 0
+from functools import reduce
 
-for i in range(len(number)):
-    if number[i].isnumeric():
-        number_int = int(number[i])
-        sum = sum + number_int
+number = (input('Введите число: '))
+#sum = 0
+
+print(reduce(lambda x, y: int(x) + int(y), list(filter(lambda x: x.isnumeric(), list(number)))))
+
+# for i in range(len(number)):
+#     if number[i].isnumeric():
+#         number_int = int(number[i])
+#         sum = sum + number_int
 print(sum)
--- Task2.py
+++ Task2.py
@@ -3,13 +3,19 @@
 # Пример:
 # Для n=4 -> [2, 2.25, 2.37, 2.44]
 # Сумма 9.06
+from functools import reduce
 
-number = int(input('Введите число: '))
-sequence = []
-sum_seq = 0
 
-for i in range(1, number+1):
-    sequence.append(round((1+1/i)**i, 2))
-    sum_seq += sequence[i-1]
-print(sequence)
-print(f'Сумма: {sum_seq}')
+sequence1 = [round((1+1/i)**i, 2) for i in range(1, int(input('Введите число: '))+1)]
+sum1 = reduce(lambda x, y: x+y, sequence1)
+print(sequence1)
+print(f'Сумма: {sum1}')
+
+# number = int(input('Введите число: '))
+# sequence = []
+# sum_seq = 0
+# for i in range(1, number+1):
+#     sequence.append(round((1+1/i)**i, 2))
+#     sum_seq += sequence[i-1]
+# print(sequence)
+# print(f'Сумма: {sum_seq}')
